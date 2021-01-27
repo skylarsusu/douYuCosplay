2766874429afca5c1a52d0bb1c7854ad4b3a1fa2
subaojing <subaojing@kinfsoft.com>
2021-01-27 12:53:38 +0800

    ios 9 以上版本利用apprance特性

---
+++ DouYUZZ/DouYUZZ/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DouYUZZ
+//
+//  Created by 苏宝敬 on 2021/1/27.
+//  Copyright © 2021 苏宝敬. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor.orange
+        return true
+    }
+
+
+
+}
+
---
+++ DouYUZZ/DouYUZZ/Classes/Main/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  DouYUZZ
+//
+//  Created by 苏宝敬 on 2021/1/27.
+//  Copyright © 2021 苏宝敬. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
